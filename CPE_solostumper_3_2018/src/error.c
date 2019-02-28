1a6fc72060a0c1f6c95ec250dfafc79bebdc01e0
Adam Cyffka <45879662+AdamCyffka@users.noreply.github.com>
2019-02-28 18:36:44 +0100

    error.c added

---
+++ CPE_solostumper_3_2018/src/error.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2019
+** error
+** File description:
+** error file
+*/
+
+#include "fuzzbozz.h"
+
+int error(void)
+{
+    my_putstr("Error: the second parameter must be");
+    my_putstr(" greater than the first one.");
+    return (84);
+}
