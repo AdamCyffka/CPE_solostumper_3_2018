ad9778296c65983365710bb9a2d71f57756cdca2
Adam Cyffka <45879662+AdamCyffka@users.noreply.github.com>
2019-03-27 22:29:46 +0100

    Update main.c

--- CPE_solostumper_3_2018/src/main.c
+++ CPE_solostumper_3_2018/src/main.c
@@ -1,6 +1,6 @@
 /*
 ** EPITECH PROJECT, 2019
-** main
+** fuzzbozz
 ** File description:
 ** main for fuzzbozz
 */
@@ -9,14 +9,15 @@
 
 int main(int ac, char **av)
 {
+    int from = my_getnbr(av[1]);
+    int to = my_getnbr(av[2]);
+
     if (ac != 3)
         return (84);
     if (my_getnbr(av[1]) > my_getnbr(av[2])) {
         error();
         return (84);
     } else {
-        int from = my_getnbr(av[1]);
-        int to = my_getnbr(av[2]);
         start(from, to);
         return (0);
     }
